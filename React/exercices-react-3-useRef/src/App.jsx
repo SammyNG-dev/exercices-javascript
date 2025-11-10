ba6287f808e3388ad76218091f96e8c6c185b15d
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2025-11-10 10:16:11 +0100

    initial commit

---
+++ React/exercices-react-3-useRef/src/App.jsx
@@ -0,0 +1,18 @@
+
+import { Link } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Ce projet a pour but de faire des exercices sur le hook useRef</h1>
+      <nav>
+        <Link className="link" to="/exo1">
+          Exercice 1 : ...
+        </Link>
+      </nav>
+    </>
+  );
+}
+
+export default App;
---
+++ React/exercices-react-3-useRef/src/pages/Exercice1.jsx
@@ -0,0 +1,5 @@
+function Exercice1(){
+    return <h1>Hello, World!</h1>
+}
+
+export default Exercice1
