900127a6fa154bbe99a8794e83f8f1d5e0e05580
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2025-12-15 16:04:27 +0100

    express - exercice 2 : Routes GET et POST avec JSON

---
+++ Express-JS/Exercice-2/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+
+const app = express();
+
+const PORT = 3000
+
+app.use(express.json());
+
+app.get("/user", (req, res) => {
+  res.json({ name: "Sammy", age: 36, hobby: "coder" });
+});
+
+app.post("/user", (req, res) => {
+  const user  = req.body;
+  res.json({ message: "utilisateur reçu", user: user });
+});
+
+app.listen(PORT, ()=>{
+    console.log(`Le serveur écoute sur le port ${PORT}`);
+})
