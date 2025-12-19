4b8d1c2c1dd8b12d666248078c6ed8f1756efa6a
SammyNG-dev <sammy.nguyenvanbau@gmail.com>
2025-12-19 14:55:11 +0100

    express - exercice 8 : Persistance des données avec un fichier JSON

--- Express-JS/Exercices/modules/user/UserRepository.js
+++ Express-JS/Exercices/modules/user/UserRepository.js
@@ -1,11 +1,18 @@
-class UserRepository {
-  users = [];
-  userId = 0;
+import fs from "node:fs";
+import { fileURLToPath } from "node:url";
+import { dirname, join } from "node:path";
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+const FILE_PATH = join(_dirname, "users.json");
 
+class UserRepository {
   createUser(newUser) {
-    this.userId += 1
-    const user = { id: this.userId, name: newUser };
-    this.users.push(user);
+    const users = JSON.parse(fs.readFileSync(FILE_PATH, "utf-8"));
+    let userId = users.length > 0 ? Math.max(...users.map((u) => u.id)) : 0;
+    userId += 1;
+    const user = { id: userId, name: newUser };
+    users.push(user);
+    fs.writeFileSync(FILE_PATH, JSON.stringify(users, null, 2));
     return user;
   }
 
